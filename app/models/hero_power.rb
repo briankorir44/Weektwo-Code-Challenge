ae125916032f370e5618d828c804171226467129
briankorir44 <briankorir44@yahoo.com>
2022-12-08 20:21:01 +0300

    create controllers

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,10 +1,10 @@
-# class HeroPower < ApplicationRecord
-#     validates :strength,
-#               inclusion: {
-#                 in: %w[Strong Weak Average],
-#                 message: "%{value} is not a valid size"
-#               }
+class HeroPower < ApplicationRecord
+    validates :strength,
+              inclusion: {
+                in: %w[Strong Weak Average],
+                message: "%{value} is not a valid size"
+              }
   
-#     belongs_to :hero
-#     belongs_to :power
-#   end
+    belongs_to :hero
+    belongs_to :power
+  end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,6 @@
 class Power < ApplicationRecord
-end
+    validates :description, presence: true, length: { minimum: 20, maximum: 400 }
+  
+    has_many :hero_powers
+    has_many :heros, through: :hero_powers
+  end
