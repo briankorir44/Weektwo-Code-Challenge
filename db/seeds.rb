213d672030f87e5842c58d58f2e14ea0032e4637
briankorir44 <briankorir44@yahoo.com>
2022-12-08 21:32:42 +0300

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+heroes = Hero.create([
+    {name: "Devis", super_name: "Davis V"},
+    {name: "Devis", super_name: "Davis V"},
+    {name: "Devis", super_name: "Davis V"},
+    {name: "Devis", super_name: "Davis V"}
+    ])
+
+powers = Power.create([
+    { name: "super", description: "fast"},
+    { name: "telepotation", description: "fast"},
+    { name: "invisibility", description: "inviscible"}
+    ])
+
+heropowers = HeroPower.create([
+    {strength: "Strong", hero_id: 1,power_id: 1},
+    {strength: "Strong", hero_id: 2,power_id: 1},
+    {strength: "Average", hero_id: 3,power_id: 2},
+    {strength: "Weak", hero_id: 4,power_id: 3}
+    ])
